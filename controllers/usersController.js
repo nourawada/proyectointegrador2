4689df6cc55bb5b6689be8b18730fbd3d4a8493b
Tomy <tfalaq@udesa.edu.ar>
2022-04-09 19:52:42 -0300

    cambios en el controller del users y + obj lit usu

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,1 +1,6 @@
 const usuario = require('../db/usuario')
+const usuarioController = {
+    index: function(req, res){
+        return res.render('usuario',{listadoUsuarios: usuario.lista})
+    }, 
+}
--- db/usuario.js
+++ db/usuario.js
@@ -1,1 +1,9 @@
-    
+/* 
+
+let usuario = {
+     usuarioNombre: "John",
+    usuarioApellido: "Doe",
+    usuarioDireccion: "Monroe 228"
+    usuarioFechaDeNacimiento: "09-12-1998"
+    usuarioDocumentoNacional: "42822428"
+}; */
