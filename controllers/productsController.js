053cb50e16225ab65c499645739335a344d00241
nourawada <nawada@udesa.edu.ar>
2022-04-10 20:43:14 -0300

    agrego el for para comentarios en product.ejs para que los comentarios sean visibles

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,11 +1,12 @@
 const productos = require('../db/products')
+const comentarios = require('../db/comentarios')
 const productController = {
     index: function(req, res){
-        return res.render('product',{listadoProductos: productos.lista})
+        return res.render('product',{productos: productos, comentarios: comentarios})
     }, 
 
 productadd: function(req, res){
-    return res.render('product-add',{listadoProductos: productos.lista})
+    return res.render('product-add',{productos: productos, comentarios: comentarios})
 }, 
 }
 
--- db/comentarios.js
+++ db/comentarios.js
@@ -2,32 +2,32 @@ var comentarios = [
     {
     nombre: "Valen123",
     comentario: "Vale la pena comprar, el funcionamiento es increible y el servicio tambien",
-    imagen: "/images/users/Valen123.jpeg",
+    imagen: "/images/products/Valen123.jpeg",
     },
     {
     nombre: "FacuMarz",
     comentario: "Uno de los mejores proveedores del pais",
-    imagen: "/images/users/FacuMarz.jpeg",
+    imagen: "/images/products/FacuMarz.jpg",
     },
     {
     nombre: "TomyF78",
     comentario: "Excelente producto. Muy buen servicio y rapidez",
-    imagen:"/images/users/TomyF78.jpeg",
+    imagen:"/images/products/TomyF78.jpeg",
     },
     {
     nombre:"JuanaS45",
     comentario:"Buen producto, acorde al precio y funciona correctamente",
-    imagen:"/images/users/JuanaS45.jpeg",
+    imagen:"/images/products/JuanaS45.jpeg",
     },
     {
     nombre:"FeliAlonso5",
     comentario:"Es excelente. Supero las expectativas, mas que conformes.",
-    imagen:"/images/users/FeliAlonso5.jpeg",
+    imagen:"/images/products/FeliAlonso5.jpeg",
     },
     {
     nombre:"LuliSanchez1",
     comentario:"Me parece genial, pero me hubiese gustado que se demoren menos",
-    imagen:"/images/users/LuliSanchez.jpeg",
+    imagen:"/images/products/LuliSanchez1.jpeg",
     },
 ]
 module.exports = comentarios
