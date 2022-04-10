4d5edd6b6d02c2be3cc19c99a8a968364b02e410
nourawada <nawada@udesa.edu.ar>
2022-04-10 20:11:49 -0300

    agrego comentarios en el db con los comentarios correspondientes y agrego imagenes de users

---
+++ db/comentarios.js
@@ -0,0 +1,33 @@
+var comentarios = [
+    {
+    nombre: "Valen123",
+    comentario: "Vale la pena comprar, el funcionamiento es increible y el servicio tambien",
+    imagen: "/images/users/Valen123.jpeg",
+    },
+    {
+    nombre: "FacuMarz",
+    comentario: "Uno de los mejores proveedores del pais",
+    imagen: "/images/users/FacuMarz.jpeg",
+    },
+    {
+    nombre: "TomyF78",
+    comentario: "Excelente producto. Muy buen servicio y rapidez",
+    imagen:"/images/users/TomyF78.jpeg",
+    },
+    {
+    nombre:"JuanaS45",
+    comentario:"Buen producto, acorde al precio y funciona correctamente",
+    imagen:"/images/users/JuanaS45.jpeg",
+    },
+    {
+    nombre:"FeliAlonso5",
+    comentario:"Es excelente. Supero las expectativas, mas que conformes.",
+    imagen:"/images/users/FeliAlonso5.jpeg",
+    },
+    {
+    nombre:"LuliSanchez1",
+    comentario:"Me parece genial, pero me hubiese gustado que se demoren menos",
+    imagen:"/images/users/LuliSanchez.jpeg",
+    },
+]
+module.exports = comentarios
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,5 @@ const indexController = require('../controllers/indexController')
 router.get('/', indexController.index);
 
 router.get('/search-results', indexController.searchresults)
+
 module.exports = router;
