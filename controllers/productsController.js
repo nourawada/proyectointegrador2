77bfc232e51d402d68a3e8e7a607058c8e104930
facumartinezz <fmartinezzorzi@udesa.edu.ar>
2022-04-10 21:59:32 -0300

    Imagenes de todos los productos con sus respectivas decripciones y nombres agregadas, product.ejs funciona, me falta agregar un if

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,12 +1,12 @@
-const productos = require('../db/products')
+const productos = require('../db/productos')
 const comentarios = require('../db/comentarios')
 const productController = {
     index: function(req, res){
-        return res.render('product',{productos: productos, comentarios: comentarios})
+         res.render('product',{productos: productos, comentarios: comentarios})
     }, 
 
 productadd: function(req, res){
-    return res.render('product-add',{productos: productos, comentarios: comentarios})
+     res.render('product-add',{productos: productos, comentarios: comentarios})
 }, 
 }
 
---
+++ db/productos.js
@@ -0,0 +1,65 @@
+ var productos =  [
+    { 
+        nombre:"placa de video geforce asus gtx 1650 4gb gddr6 phoenix oc" ,
+        imagen: "/images/products/placa-geforce-gtx-1650-gddr6.jpg",
+        descripcion: "está dedicada al procesamiento de datos relacionados con el vídeo y las imágenes que se están reproduciendo en el ordenador.",
+        fechaDeCarga: 2022,  
+    }, 
+   /* { 
+        nombre:"Gabinete Kolink Void ARGB ATX Vidrio Templado" ,
+        imagen: "/images/products/gabineteNegro.jpg" ,
+        descripcion: "armazón que contiene los componentes que integran una computadora (tarjeta madre, procesador, memoria RAM, dispositivos periféricos, etc.).",
+        fechaDeCarga: 2021,  
+    },
+    { 
+        nombre: "Teclado Mecanico ASUS TUF Gaming K3 US Red" ,
+        imagen: "/images/products/teclagoAsus.jpg",
+        descripcion: "un dispositivo de entrada que utiliza un sistema de puntadas o márgenes, para que actúen como palancas mecánicas o interruptores electrónicos que envían toda la información a la computadora.",
+        fechaDeCarga: 2018,  
+    },
+    { 
+        nombre: "Procesador AMD Ryzen 5 1600 AF Zen+ 12nm AM4 Wraith Stealth Cooler",
+        imagen: "/images/products/procesadorAmd.jpg",
+        descripcion: "es la unidad de procesamiento encargada de interpretar las instrucciones de un hardware haciendo uso de distintas operaciones aritméticas y matemáticas.",
+        fechaDeCarga: 2018,  
+    },
+    { 
+        nombre: "Silla Gamer Cooler Master Caliber R2C Grey",
+        imagen: "/images/products/sillaGamer.jpg",
+        descripcion: "Estas sillas  corrigen los problemas de la columna con ciencia ergonómica simple. En lugar de que su columna vertebral sostenga su cuerpo contra la gravedad, las sillas de juego hacen el trabajo por usted. Un respaldo alto acolchado con cojines lumbares y para el cuello proporciona el soporte principal.",
+        fechaDeCarga: 2019,  
+    },
+    { 
+        nombre: "Memoria GeiL DDR4 16GB (2x8GB) 3200MHz EVO X II RGB Black " ,
+        imagen: "/images/products/memoraRam.jpg",
+        descripcion: "una nueva serie de memorias RAM DDR4 que pretende ser una opción mas para los jugadores de PC, que necesitan unos kits con la mayor velocidad posible, y así sacar el mayor rendimiento del PC.",
+        fechaDeCarga: 2022,  
+    },
+    { 
+        nombre: "Auriculares HP HyperX Cloud II Pro Gun Metal PC",
+        imagen: "/images/products/aurisGamers.jpg",
+        descripcion:"tu auricular gaming será un transductor de algo más que de música. Sonidos graves, agudos sin distorsión (drivers), sensibilidad del altavoz y frecuencia disponible, hacen que la calidad del sonido sea buena" ,
+        fechaDeCarga: 2020,  
+    },
+    { 
+        nombre: "Monitor Samsung 24 Curvo F390" ,
+        imagen: "/images/products/monitorGamer.jpg",
+        descripcion: "esta diseñado para las necesidades muy específicas de los videojuegos. Tiene paneles con alta velocidad de respuesta y frecuencias de actualización. Mantienen el input lag al mínimo y no agregan ninguna molestia a tu imagen.",
+        fechaDeCarga: 2021,  
+    },
+    { 
+        nombre: "Mouse Redragon M710 Memeanlion Chroma RGB 10,000dpi ",
+        imagen: "/images/products/mouseGamer.jpg",
+        descripcion: "estos mouses son visualmente mucho más atractivos, ideales para complementarlos con los notebooks gamers. Suelen tener más luces que los tradicionales, especialmente en los laterales. Además, cuentan con muchas más funciones y botones.",
+        fechaDeCarga: 2022,  
+    },
+    { 
+        nombre: "Disco Solido SSD Externo Team Treasure Touch RGB 1TB 400MB/s ",
+        imagen: "images/products/discoSolido.jpg",
+        descripcion: "El disco solido es una nueva generación de dispositivos de almacenamiento que se emplea en equipos. Estos reemplazan los discos duros mecánicos tradicionales con una memoria basada en flash, significativamente más rápida.",
+        fechaDeCarga: 2019,  
+    },*/ 
+    ]
+
+    module.exports = productos
+
