6eaef1b91ef4b00cc2cc14f8f55f6b7b1d943a13
nourawada <nawada@udesa.edu.ar>
2022-04-15 23:14:42 -0300

    Hice el headerLogeado. Tambien agregue variable en el productController para poder utilizar el usuario.

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,12 +1,14 @@
 const productos = require('../db/productos')
 const comentarios = require('../db/comentarios')
+const usuario = require('../db/usuario')
+
 const productController = {
     index: function(req, res){
          res.render('product',{productos: productos, comentarios: comentarios})
     }, 
 
 productadd: function(req, res){
-     res.render('product-add',{productos: productos, comentarios: comentarios})
+     res.render('product-add',{productos: productos, comentarios: comentarios, usuario: usuario})
 }, 
      productId: function(req,res){
     res.render ('product',{productos:productos, productId: req.params.id, comentarios: comentarios})
