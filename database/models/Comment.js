38f5ea939be5e6355adcd3b7c9aed190afb2d6c5
nourawada <nawada@udesa.edu.ar>
2022-06-11 17:04:09 -0300

    agrego Comment en los modelos

---
+++ database/models/Comment.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "Comment";
+
+    let cols = {
+        id:{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        text:{
+            type: dataTypes.STRING
+        },
+        products_id:{
+            type: dataTypes.INTEGER
+        },
+        users_id:{
+            type: dataTypes.INTEGER
+        },
+        createdAt:{
+            type: dataTypes.DATE,
+            notNull: true
+        },
+        updatedAt:{
+            type: dataTypes.DATE,
+            notNull: true
+        }
+    };
+    //configuraciones adicionales. 
+    let config = {
+        tableName: "comments",
+        timestamp: true,
+        underscored:false
+    }
+
+    const Comments = sequelize.define (alias, cols, config);
+
+    return Comments;
+}
