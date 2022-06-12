8560bdee03dcfb6bcfcb8fdd506e92185deaa1a5
nourawada <nawada@udesa.edu.ar>
2022-06-12 17:21:38 -0300

    agrego el mysql corregido, el indexController con el metodo index hecho y correccion en el views

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,19 @@
-const productos = require('../db/productos')
+const db = require('../database/models');
+const products = db.Product
+const users = db.Users
+const op = db.Sequelize.Op;
+
+//const productos = require('../db/productos')
 
 const indexController = {
 index:function(req, res) {
-    res.render('index', {'productos': productos});
-},
-searchresults:function(req, res) {
-    res.render('search-results', {'productos':productos});
-},
+    products.findAll()
+    .then(function(productos){
+        return res.render('index', {products:productos});
+      })
+    },
+//searchresults:function(req, res) {
+//    res.render('search-results', {'productos':productos});
+//},
 };
 module.exports = indexController;
--- database/mysql/scriptDB.sql
+++ database/mysql/scriptDB.sql
@@ -19,7 +19,7 @@ name VARCHAR(250) NOT NULL,
 image VARCHAR(250) NOT NULL,
 brand VARCHAR(250) NOT NULL,
 descripcion VARCHAR(500) NOT NULL,
-users_id INT,
+users_id INT UNSIGNED NOT NULL,
 createdAt DATE,
 updatedAt DATE,
 
@@ -29,8 +29,8 @@ FOREIGN KEY (users_id) REFERENCES users(id)
 CREATE TABLE comments (
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, 
 text VARCHAR(250) NOT NULL, 
-products_id INT,
-users_id INT,
+products_id INT UNSIGNED NOT NULL,
+users_id INT UNSIGNED NOT NULL,
 createdAt DATE,
 updatedAt DATE,
 
@@ -40,31 +40,32 @@ FOREIGN KEY(users_id) REFERENCES users(id)
 
 CREATE TABLE user_Followers (
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-followers_id INT,
-users_id INT,
+followers_id INT UNSIGNED NOT NULL,
+users_id INT UNSIGNED NOT NULL,
 
 FOREIGN KEY(users_id) REFERENCES users(id),
-FOREIGN KEY(followers_id) REFERENCES followers(id)
+FOREIGN KEY(followers_id) REFERENCES users(id)
 );
-
-INSERT INTO products (name, image, brand, descripcion , users_id, createdAt, updatedAt)
-VALUES ('Placa de video', '/images/products/1.jpeg' , 'MSI', 'Está dedicada al procesamiento de datos relacionados con el vídeo y las imágenes que se están reproduciendo en el ordenador',1, curdate(), curdate()),
-('Gabinete Kolink Void ARGB ATX Vidrio Templado', '/images/products/2.jpg', 'KOLINK', 'Armazón que contiene los componentes que integran una computadora (tarjeta madre, procesador, memoria RAM, dispositivos periféricos, etc.)',1, curdate(), curdate()),
-('Teclado Mecanico ASUS TUF Gaming K3 US Red','/images/products/3.jpg','ASUS','Un dispositivo de entrada que utiliza un sistema de puntadas o márgenes, para que actúen como palancas mecánicas o interruptores electrónicos que envían toda la información a la computadora',1, curdate(), curdate()),
-('Procesador AMD Ryzen 5 1600 AF Zen+ 12nm AM4 Wraith Stealth Cooler','/images/products/4.jpg','AMD','Es la unidad de procesamiento encargada de interpretar las instrucciones de un hardware haciendo uso de distintas operaciones aritméticas y matemáticas',1, curdate(), curdate()),
-('Silla Gamer Cooler Master Caliber R2C Grey','/images/products/5.jpg','Cooler Master','Estas sillas  corrigen los problemas de la columna con ciencia ergonómica simple. En lugar de que su columna vertebral sostenga su cuerpo contra la gravedad, las sillas de juego hacen el trabajo por usted. Un respaldo alto acolchado con cojines lumbares y para el cuello proporciona el soporte principal',1, curdate(), curdate()),
-('Memoria GeiL DDR4 16GB (2x8GB) 3200MHz EVO X II RGB Black','/images/products/6.jpg','GeiL','Una nueva serie de memorias RAM DDR4 que pretende ser una opción mas para los jugadores de PC, que necesitan unos kits con la mayor velocidad posible, y así sacar el mayor rendimiento del PC',1, curdate(), curdate()),
-('Auriculares HP HyperX Cloud II Pro Gun Metal PC','/images/products/7.jpg','HyperX','Tu auricular gaming será un transductor de algo más que de música. Sonidos graves, agudos sin distorsión (drivers), sensibilidad del altavoz y frecuencia disponible, hacen que la calidad del sonido sea buena',1, curdate(), curdate()),
-('Monitor Samsung 24 Curvo F390','/images/products/8.jpg','Samsung','Esta diseñado para las necesidades muy específicas de los videojuegos. Tiene paneles con alta velocidad de respuesta y frecuencias de actualización. Mantienen el input lag al mínimo y no agregan ninguna molestia a tu imagen',1, curdate(), curdate()),
-('Mouse Redragon M710 Memeanlion Chroma RGB 10,000dpi','/images/products/9.jpg','Redragon','Estos mouses son visualmente mucho más atractivos, ideales para complementarlos con los notebooks gamers. Suelen tener más luces que los tradicionales, especialmente en los laterales. Además, cuentan con muchas más funciones y botones,',1, curdate(), curdate()),
-('Disco Solido SSD Externo Team Treasure Touch RGB 1TB 400MB/s','/images/products/10.jpg','Team','El disco solido es una nueva generación de dispositivos de almacenamiento que se emplea en equipos. Estos reemplazan los discos duros mecánicos tradicionales con una memoria basada en flash, significativamente más rápida',1, curdate(), curdate()),
-('Mother Asrock B365 Phantom Gaming 4 9th Gen 1151 M.2 RGB ','/images/products/11.jpg','Asrock','La placa mother es una tarjeta de circuito impreso a la que se conectan los componentes que constituyen la computadora',1, curdate(), curdate()),
-('Cooler CPU ID-Cooling FROSTFLOW X 240 CPU Intel 1151 & AMD AM4','/images/products/12.jpg','ID-Cooling','Un cooler, es un ventilador capaz de enfriar y mantener la temperatura, ni más ni menos. Estos cooler tienden a utilizar para todo tipo de aspectos de la vida, y no sólo están presentes en el mundo de la informatica',1, curdate(), curdate());
-
 
 INSERT INTO users (email, username, password, image, createdAt, updatedAt)
 VALUES ('nawada@udesa.edu.ar','nourawada','nour123','/images/users/homero.jpeg',curdate(), curdate()),
 ('facundomartinezz@udesa.edu.ar','facumartinez', 'facu2022', '/images/users/morty1.jpeg',curdate(), curdate()),
 ('tomyfalaq@udesa.edu.ar','tomyfalaq', 'riverelmejor', '/images/users/dwight1.jpeg', curdate(), curdate());
+
+INSERT INTO products (name, image, brand, descripcion ,createdAt, updatedAt, users_id)
+VALUES ('Placa de video', '/images/products/1.jpeg' , 'MSI', 'Está dedicada al procesamiento de datos relacionados con el vídeo y las imágenes que se están reproduciendo en el ordenador', curdate(), curdate(),1),
+('Gabinete Kolink Void ARGB ATX Vidrio Templado', '/images/products/2.jpg', 'KOLINK', 'Armazón que contiene los componentes que integran una computadora (tarjeta madre, procesador, memoria RAM, dispositivos periféricos, etc.)', curdate(), curdate(),1),
+('Teclado Mecanico ASUS TUF Gaming K3 US Red','/images/products/3.jpg','ASUS','Un dispositivo de entrada que utiliza un sistema de puntadas o márgenes, para que actúen como palancas mecánicas o interruptores electrónicos que envían toda la información a la computadora', curdate(), curdate(),1),
+('Procesador AMD Ryzen 5 1600 AF Zen+ 12nm AM4 Wraith Stealth Cooler','/images/products/4.jpg','AMD','Es la unidad de procesamiento encargada de interpretar las instrucciones de un hardware haciendo uso de distintas operaciones aritméticas y matemáticas', curdate(), curdate(),1),
+('Silla Gamer Cooler Master Caliber R2C Grey','/images/products/5.jpg','Cooler Master','Estas sillas  corrigen los problemas de la columna con ciencia ergonómica simple. En lugar de que su columna vertebral sostenga su cuerpo contra la gravedad, las sillas de juego hacen el trabajo por usted. Un respaldo alto acolchado con cojines lumbares y para el cuello proporciona el soporte principal',curdate(), curdate(),1),
+('Memoria GeiL DDR4 16GB (2x8GB) 3200MHz EVO X II RGB Black','/images/products/6.jpg','GeiL','Una nueva serie de memorias RAM DDR4 que pretende ser una opción mas para los jugadores de PC, que necesitan unos kits con la mayor velocidad posible, y así sacar el mayor rendimiento del PC', curdate(), curdate(),1),
+('Auriculares HP HyperX Cloud II Pro Gun Metal PC','/images/products/7.jpg','HyperX','Tu auricular gaming será un transductor de algo más que de música. Sonidos graves, agudos sin distorsión (drivers), sensibilidad del altavoz y frecuencia disponible, hacen que la calidad del sonido sea buena', curdate(), curdate(),1),
+('Monitor Samsung 24 Curvo F390','/images/products/8.jpg','Samsung','Esta diseñado para las necesidades muy específicas de los videojuegos. Tiene paneles con alta velocidad de respuesta y frecuencias de actualización. Mantienen el input lag al mínimo y no agregan ninguna molestia a tu imagen', curdate(), curdate(),1),
+('Mouse Redragon M710 Memeanlion Chroma RGB 10,000dpi','/images/products/9.jpg','Redragon','Estos mouses son visualmente mucho más atractivos, ideales para complementarlos con los notebooks gamers. Suelen tener más luces que los tradicionales, especialmente en los laterales. Además, cuentan con muchas más funciones y botones,', curdate(), curdate(),1),
+('Disco Solido SSD Externo Team Treasure Touch RGB 1TB 400MB/s','/images/products/10.jpg','Team','El disco solido es una nueva generación de dispositivos de almacenamiento que se emplea en equipos. Estos reemplazan los discos duros mecánicos tradicionales con una memoria basada en flash, significativamente más rápida', curdate(), curdate(),1),
+('Mother Asrock B365 Phantom Gaming 4 9th Gen 1151 M.2 RGB ','/images/products/11.jpg','Asrock','La placa mother es una tarjeta de circuito impreso a la que se conectan los componentes que constituyen la computadora',curdate(), curdate(),1),
+('Cooler CPU ID-Cooling FROSTFLOW X 240 CPU Intel 1151 & AMD AM4','/images/products/12.jpg','ID-Cooling','Un cooler, es un ventilador capaz de enfriar y mantener la temperatura, ni más ni menos. Estos cooler tienden a utilizar para todo tipo de aspectos de la vida, y no sólo están presentes en el mundo de la informatica', curdate(), curdate(),1);
+
+
 
 
